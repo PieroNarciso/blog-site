5f0a94cab2c29156a531908d54823353da5f964f
Piero Narciso <pieronarciso988@gmail.com>
2021-04-17 17:43:35 -0500

    Setup template for individual blogs

---
+++ src/templates/BlogTemplate.js
@@ -0,0 +1,39 @@
+import { graphql } from "gatsby";
+import React from "react";
+
+import Layout from "../components/Layout";
+
+export default function BlogTemplate({ data }) {
+  const { markdownRemark } = data;
+  const { frontmatter, html } = markdownRemark;
+  return (
+    <Layout>
+      <div className="mx-4 sm:mx-auto mt-4 mb-4">
+        <h1 className="text-center text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          {frontmatter.title}
+        </h1>
+        <hr className="mt-4 border-t-2 w-20 mx-auto" />
+        <span className="text-sm text-center text-gray-600 tracking-wide font-medium w-full block">
+          {frontmatter.date}
+        </span>
+        <div
+          className="prose lg:prose-lg mt-3 mx-auto"
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
+      </div>
+    </Layout>
+  );
+}
+
+export const pageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(frontmatter: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "MMMM DD, YYYY")
+        slug
+        title
+      }
+    }
+  }
+`;
